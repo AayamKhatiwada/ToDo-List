dcc66ea9c2e43c63c7c9054d0fe42c879f9fa6fb
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-06-22 17:13:59 +0545

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,27 +1,35 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  
+class App extends React.Component {
 
-  return (
-    <div className="App">
-      <div id="myDIV" className="header">
-        <h2>My To Do List</h2>
-        <input type="text" id="myInput" placeholder="Title..." />
-        <span onclick="newElement()" className="addBtn">Add</span>
+  componentDidMount() {
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = ul.getElementsByTagName('li');
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div id="myDIV" className="header">
+          <h2>My To Do List</h2>
+          <input type="text" id="myInput" placeholder="Title..." />
+          <span onclick="newElement()" className="addBtn">Add</span>
+          <span></span>
+        </div>
+
+        <ul id="myUL">
+          <li>Hit the gym</li>
+          <li class="checked">Pay bills</li>
+          <li>Meet George</li>
+          <li>Buy eggs</li>
+          <li>Read a book</li>
+          <li>Organize office</li>
+        </ul>
+
       </div>
-
-      <ul id="myUL">
-        <li>Hit the gym</li>
-        <li class="checked">Pay bills</li>
-        <li>Meet George</li>
-        <li>Buy eggs</li>
-        <li>Read a book</li>
-        <li>Organize office</li>
-      </ul>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
